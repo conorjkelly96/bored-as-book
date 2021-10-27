453c7fef7ff2ce12a3b35122573bb427481cadb6
Conor Kelly <conorjkelly96@gmail.com>
2021-10-27 20:47:02 +0100

    added resetCSS and handleChoice

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,6 @@
+const choicesContainer = $("#choices-container");
+const baseURL = "https://www.boredapi.com";
+
 const toggleNavBar = function () {
   // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
   $(".navbar-burger").toggleClass("is-active");
@@ -9,4 +12,33 @@ const onReady = function () {
   $(".navbar-burger").click(toggleNavBar);
 };
 
+const resetChoicesCSS = function (buttons) {
+  const removeSuccessClass = function (button) {
+    if (button.hasClass("is-success")) {
+      button.removeClass("is-success");
+    }
+  };
+  console.log(buttons);
+  buttons.forEach(removeSuccessClass);
+};
+
+const handleSelectedChoice = function (event) {
+  const target = $(event.target);
+  const currentTarget = $(event.currentTarget);
+
+  // get selected target if target is a button, then get the data-category of button
+  if (target.is("button")) {
+    const categorySelected = target.data("category");
+    // go through buttons to find 'is-success' class and remove
+    const choicesButtons = currentTarget.find("button");
+    resetChoicesCSS(choicesButtons);
+    target.addClass("is-success");
+    const url =
+      categorySelected === "random"
+        ? `${baseURL}/api/activity`
+        : `${baseURL}/api/activity?type=${categorySelected}`;
+  }
+};
+
+choicesContainer.on("click", handleSelectedChoice);
 $(document).ready(onReady);
