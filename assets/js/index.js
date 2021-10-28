476eb8309ba99e2df8ae57854802fe704d0b6e91
surajverma2587 <surajverma2587@gmail.com>
2021-10-28 20:09:50 +0100

    refactor and fix bug

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,19 +1,14 @@
 const choicesContainer = $("#choices-container");
 const cardContainer = $("#card-container");
+const navBurger = $(".navbar-burger");
 const baseURL = "https://www.boredapi.com";
-let currentCategory;
 // if making API call this sets to TRUE, else FALSE
 let currentStatus;
 
 const toggleNavBar = function () {
   // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
-  $(".navbar-burger").toggleClass("is-active");
+  navBurger.toggleClass("is-active");
   $(".navbar-menu").toggleClass("is-active");
-};
-
-const onReady = function () {
-  // Check for click events on the navbar burger icon
-  $(".navbar-burger").click(toggleNavBar);
 };
 
 const resetChoicesCSS = function (buttons) {
@@ -32,40 +27,39 @@ const getApiCall = async function (url) {
   return data.json();
 };
 
+const renderActivityCard = async function (categorySelected) {
+  const url =
+    categorySelected === "random"
+      ? `${baseURL}/api/activity`
+      : `${baseURL}/api/activity?type=${categorySelected}`;
+  const data = await getApiCall(url);
+  constructActivityCard(data);
+};
+
 const handleSelectedChoice = async function (event) {
   const target = $(event.target);
   const currentTarget = $(event.currentTarget);
 
-  // get selected target if target is a button, then get the data-category of button
   if (target.is("button")) {
-    const categorySelected = target.data("category");
-    currentCategory = categorySelected;
-    // go through buttons to find 'is-success' class and remove
-    const choicesButtons = currentTarget.find("button");
-    // add spread operator to the choices button array to loop over the buttons
-    resetChoicesCSS([...choicesButtons]);
+    // reset the selected button
+    resetChoicesCSS([...currentTarget.find("button")]);
+
+    // set the selected button to green
     target.addClass("is-success");
-    const url =
-      categorySelected === "random"
-        ? `${baseURL}/api/activity`
-        : `${baseURL}/api/activity?type=${categorySelected}`;
-    const data = await getApiCall(url);
-    constructActivityCard(data);
+
+    // render the selected activity card
+    const categorySelected = target.data("category");
+    renderActivityCard(categorySelected);
   }
 };
 
 const handleUserChoices = async function (event) {
   const target = $(event.target);
+
   if (target.is("button")) {
+    // render the selected activity card
     const categorySelected = target.data("category");
-    // CK 28/10: see ticket 24 in Project Board
-    const url =
-      categorySelected === "random"
-        ? `${baseURL}/api/activity`
-        : `${baseURL}/api/activity?type=${categorySelected}`;
-    const data = await getApiCall(url);
-    console.log(url);
-    constructActivityCard(data);
+    renderActivityCard(categorySelected);
   }
 };
 
@@ -73,30 +67,38 @@ const handleUserChoices = async function (event) {
 const constructActivityCard = function (activity) {
   const activityParent = $("#card-container");
 
-  const activityCard = `      <div class="card ml-3 mr-3">
-<div class="card-content">
-  <p id="activity-name" class="activity-text">
-    ${activity.activity}
-  </p>
-  <p id="activity-type" class="activity-text">${activity.type}</p>
-  <p id="activity-cost" class="activity-text">Cost Index: ${
-    activity.price * 10
-  }/10</p>
-</div>
-<div class="card-footer">
-  <button data-choice="no" id="no-btn" class="card-footer-item button is-danger">
-    No
-  </button>
-  <button data-choice="yes" id="yes-btn" class="card-footer-item button is-success">
-    Yes
-  </button>
-</div>
-</div>`;
+  const activityCard = `<div class="card ml-3 mr-3">
+    <div class="card-content">
+      <p id="activity-name" class="activity-text">
+        ${activity.activity}
+      </p>
+      <p id="activity-type" class="activity-text">${activity.type}</p>
+      <p id="activity-cost" class="activity-text">Cost Index: ${
+        activity.price * 10
+      }/10</p>
+    </div>
+    <div class="card-footer">
+      <button data-choice="no" id="no-btn" class="card-footer-item button is-danger" data-category=${
+        activity.type
+      }>
+        No
+      </button>
+      <button data-choice="yes" id="yes-btn" class="card-footer-item button is-success" data-category=${
+        activity.type
+      }>
+        Yes
+      </button>
+    </div>
+  </div>`;
 
   activityParent.empty();
   activityParent.append(activityCard);
 };
 
-choicesContainer.on("click", handleSelectedChoice);
-cardContainer.on("click", handleUserChoices);
+const onReady = function () {
+  navBurger.on("click", toggleNavBar);
+  choicesContainer.on("click", handleSelectedChoice);
+  cardContainer.on("click", handleUserChoices);
+};
+
 $(document).ready(onReady);
