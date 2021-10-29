b767378cf583458a0767e73b69c2d5421904908b
Andrada Gacichevici <andradagacichevici@gmail.com>
2021-10-29 13:02:24 +0100

    render user choices/search results js

--- assets/js/myactivities.js
+++ assets/js/myactivities.js
@@ -6,5 +6,21 @@ const userchoices = [
 
 //rendering choices from local storage
 const constructUserChoices = function () {
-    const choicesParent = 
+  const choicesParent = $("#user-choices-list");
+
+  const userChoiceOption = `<li class="list-item button is-link">${userChoices[0]}</li>`;
+};
+
+const constructSearchResults = function () {
+  const searchParent = $("#search-container");
+
+  const searchResults = `<div class="tile is-child box">
+  <p class="title">Search Result</p>
+  <p>
+    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Libero
+    facilis aliquid similique sint, reiciendis cumque inventore rerum
+    unde eligendi iste est a optio. Velit magnam voluptatem eius quis
+    sit molestias?
+  </p>
+</div>`;
 };
