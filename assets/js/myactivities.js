ba5f2809572a00affc24d77edb045541131b7022
Andrada Gacichevici <andradagacichevici@gmail.com>
2021-10-29 12:52:19 +0100

    changes html (my actyivities page)

---
+++ assets/js/myactivities.js
@@ -0,0 +1,10 @@
+const userchoices = [
+  "Play footbal",
+  "Cook an Itallian meal",
+  "Go to the cinema",
+];
+
+//rendering choices from local storage
+const constructUserChoices = function () {
+    const choicesParent = 
+};
