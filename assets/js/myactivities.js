7d7fae72a30ef1192c7ecb2585198aec78f877a2
Conor Kelly <conorjkelly96@gmail.com>
2021-10-29 14:55:07 +0100

    pushing lines with API call

--- assets/js/myactivities.js
+++ assets/js/myactivities.js
@@ -1,10 +1,22 @@
-console.log("linked");
-
 const userChoices = [
   "Play football",
   "Cook an Italian meal",
   "Go to the cinema",
 ];
+
+// BELOW ARE THE FOLLOWING INPUTS TO THE API URL
+const API_KEY = "bc1d02851emsh853dc79af4fea2cp1e4839jsned8c0b9e23af";
+
+const pageNumber = 1;
+
+const pageSize = 10;
+
+const autoCorrect = true;
+
+const BASE_URL =
+  "https://contextualwebsearch-websearch-v1.p.rapidapi.com/api/Search/";
+
+const q = "covid%2019";
 
 const mockSearchResults = {
   id: "5496466817154867838",
@@ -19,6 +31,36 @@ const mockSearchResults = {
   language: "en",
   isSafe: true,
   datePublished: "0001-01-01T00:00:00",
+};
+
+// Conor 29/10: Why does this API require ajax json method to evoke a response?
+// please see documentation: https://api.jquery.com/jquery.ajax/
+const settings = {
+  // CK: this is an asynchronous function
+  async: true,
+  // see CORS documentation https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
+  crossDomain: true,
+  // URL for the API request
+  url: "https://contextualwebsearch-websearch-v1.p.rapidapi.com/api/Search/WebSearchAPI?q=covid%2019&pageNumber=1&pageSize=10&autoCorrect=true&safeSearch=true",
+  method: "GET",
+  // Header Objects: https://developer.mozilla.org/en-US/docs/Web/API/Headers
+  headers: {
+    "x-rapidapi-host": "contextualwebsearch-websearch-v1.p.rapidapi.com",
+    "x-rapidapi-key": "bc1d02851emsh853dc79af4fea2cp1e4839jsned8c0b9e23af",
+  },
+};
+
+// const apiCall = $.ajax(settings).done(function (response) {
+//   const apiCall = response;
+//   console.log(apiCall);
+// });
+
+const getApiCall = async function (url) {
+  const data = $.ajax(settings).done(function (response) {
+    const apiCall = response;
+    console.log(apiCall);
+    return apiCall;
+  });
 };
 
 //rendering choices from local storage
@@ -47,6 +89,7 @@ const constructSearchResults = function (results) {
 const onReady = function () {
   constructSearchResults(mockSearchResults);
   constructUserChoices(userChoices);
+  getApiCall();
 };
 
 $(document).ready(onReady);
