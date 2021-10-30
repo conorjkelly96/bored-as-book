85aee8417555244d405fe8a877c7822e3ea959e6
Conor Kelly <conorjkelly96@gmail.com>
2021-10-30 10:43:07 +0100

    response envoked with API Call

--- assets/js/myactivities.js
+++ assets/js/myactivities.js
@@ -15,7 +15,7 @@ const autoCorrect = true;
 
 const BASE_URL = "https://contextualwebsearch-websearch-v1.p.rapidapi.com";
 
-const query = "covid%2019";
+const query = "mancheser united";
 
 let i;
 
@@ -40,54 +40,114 @@ myHeaders.append("x-rapidapi-host", `${BASE_URL}`);
 myHeaders.append("x-rapidapi-key", `${API_KEY}`);
 
 const settings = {
-  async: true,
-  crossDomain: true,
-  method: "GET",
-  headers: myHeaders,
+  // async: true,
+  // crossDomain: true,
+  // method: "GET",
+  // headers: myHeaders,
+  headers: {
+    "x-rapidapi-host": "contextualwebsearch-websearch-v1.p.rapidapi.com",
+    "x-rapidapi-key": "bc1d02851emsh853dc79af4fea2cp1e4839jsned8c0b9e23af",
+  },
 };
+
+// // SEE WEEK 6 ACTIVITY 5 FOR AJAX API CALLS
+// Conor 29/10: Why does this API require ajax json method to evoke a response?
+// Please read the API: https://rapidapi.com/contextualwebsearch/api/web-search/ under code snippits > javascript jQuery
+// please see documentation: https://api.jquery.com/jquery.ajax/
+// const settings = {
+//   // CK: this is an asynchronous function
+//   async: true,
+//   // see CORS documentation https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
+//   crossDomain: true,
+//   // URL for the API request
+//   url: `https://contextualwebsearch-websearch-v1.p.rapidapi.com/api/Search/WebSearchAPI?q=${query}&pageNumber=1&pageSize=10&autoCorrect=true&safeSearch=true`,
+//   method: "GET",
+//   // Header Objects: https://developer.mozilla.org/en-US/docs/Web/API/Headers
+//   headers: {
+//     "x-rapidapi-host": "contextualwebsearch-websearch-v1.p.rapidapi.com",
+//     "x-rapidapi-key": "bc1d02851emsh853dc79af4fea2cp1e4839jsned8c0b9e23af",
+//   },
+// };
 
 // CK: the response has been tested with the const query variable
 const getApiCall = async function (activity) {
+  console.log(myHeaders);
   try {
     const url = `${BASE_URL}/api/Search/WebSearchAPI?q=${activity}&pageNumber=1&pageSize=10&autoCorrect=true&safeSearch=true`;
     const data = await fetch(url, settings);
     console.log(data);
-    return await data.json();
+    return data.json();
   } catch (error) {
     console.log(error);
   }
 };
+
+// const getApiCall = async function (activity) {
+//   try {
+//     const url = `${BASE_URL}/api/Search/WebSearchAPI?q=${activity}&pageNumber=1&pageSize=10&autoCorrect=true&safeSearch=true`;
+//     const data = await fetch(url, settings);
+//     console.log(data);
+//     return await data.json();
+//   } catch (error) {
+//     console.log(error);
+//   }
+// };
 
 const getChoicesFromLocalStorage = function () {
   const choices = [JSON.parse(localStorage.getItem("myActivities"))];
 };
 
 //rendering choices from local storage
+// const constructUserChoices = function (data) {
+//   const datafromLS = [...JSON.parse(localStorage.getItem("myActivities"))];
+
+//   // for each activity, create a list item and append to the list item parent
+//   const renderUserChoices = function (choice) {
+//     const choicesParent = $("#user-choices-list");
+//     const userChoiceOption = `<li class="list-item button is-link">${choice}</li>`;
+//     choicesParent.append(userChoiceOption);
+//   };
+//   console.log(datafromLS);
+
+//   datafromLS.forEach(renderUserChoices);
+// };
+
 const constructUserChoices = function (data) {
   // get the my activity object and get all the values in their array and flatten the array to make it one
-  const datafromLS = [
-    ...Object.values(JSON.parse(localStorage.getItem("myActivities"))),
-  ].flat();
+  const datafromLS = [...JSON.parse(localStorage.getItem("myActivities"))];
 
   const choicesParent = $("#user-choices-list");
   choicesParent.on("click", handleActivitySelection);
   // for each activity, create a list item and append to the list item parent
   const renderUserChoices = function (choice) {
-    const text = choice.activity.toLowerCase();
+    const text = choice;
     console.log(choice.activity);
     const userChoiceOption =
       '<li data-activity="' +
       text +
       "\" class='list-item button is-link' >" +
-      choice.activity +
-      "</li>";
+      text;
+    ("</li>");
     choicesParent.append(userChoiceOption);
   };
 
   datafromLS.forEach(renderUserChoices);
 };
 
-// cfunction to listen to even listener from the selected activity
+// const constructSearchResults = function (results) {
+//   const searchParent = $("#search-container");
+
+//   const searchResults = `<div class="tile is-child box">
+//   <p class="title">${results.title}</p>
+//   <p>
+//   ${results.description}
+//   </p>
+//   <a>${results.url}</a>
+//   </div>`;
+//   //searchParent.empty();
+//   searchParent.append(searchResults);
+// };
+
 const handleActivitySelection = async (event) => {
   const target = $(event.target);
   if (target.is("li")) {
@@ -96,9 +156,10 @@ const handleActivitySelection = async (event) => {
     const suggestedActivity = (await getApiCall(activity)) || [];
     console.log(suggestedActivity);
     // construct activity card here with the response
-    constructSearchActivity(suggestedActivity);
+    constructSearchActivity(suggestedActivity.value);
   }
 };
+
 const constructSearchActivity = function (results) {
   const searchParent = $("#search-container");
   const constructSearchResults = function (result) {
@@ -109,16 +170,16 @@ const constructSearchActivity = function (results) {
   </p>
   <a>${result.url}</a>
   </div>`;
-    //searchParent.empty();
     searchParent.append(searchResult);
   };
+  searchParent.empty();
   results.forEach(constructSearchResults);
 };
-
 const onReady = function () {
   // constructSearchResults(mockSearchResults);
   constructUserChoices(userChoices);
   getChoicesFromLocalStorage();
+  // console.log(getApiCall());
 };
 
 $(document).ready(onReady);
